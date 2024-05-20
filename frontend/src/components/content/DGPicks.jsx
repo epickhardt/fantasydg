f65599d032a889e8aa866bca25e1e14cf4e34aa0
Eliot Pickhardt <epickhardt@wisc.edu>
2024-05-20 15:11:24 -0500

    picks screen changed

--- frontend/src/components/content/DGPicks.jsx
+++ frontend/src/components/content/DGPicks.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect, useContext } from "react";
-import { Button, Dropdown, DropdownButton, DropdownItem } from "react-bootstrap";
+import React, { useState, useEffect, useContext } from "react";
+import { Button, Dropdown, DropdownButton, DropdownItem, Form } from "react-bootstrap";
 import { useNavigate } from "react-router";
 
 import DGPlayersContext from "../context/DGPlayersContext";
@@ -118,13 +118,58 @@ function DGPicks(props) {
         })
     }
 
+    const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+        <a
+          href=""
+          ref={ref}
+          onClick={(e) => {
+            e.preventDefault();
+            onClick(e);
+          }}
+        >
+          {children}
+          &#x25bc;
+        </a>
+      ));
+      
+      // forwardRef again here!
+      // Dropdown needs access to the DOM of the Menu to measure it
+      const CustomMenu = React.forwardRef(
+        ({ children, style, className, 'aria-labelledby': labeledBy }, ref) => {
+          const [value, setValue] = useState('');
+      
+          return (
+            <div
+              ref={ref}
+              style={style}
+              className={className}
+              aria-labelledby={labeledBy}
+            >
+              <Form.Control
+                autoFocus
+                className="mx-3 my-2 w-auto"
+                placeholder="Type to filter..."
+                onChange={(e) => setValue(e.target.value)}
+                value={value}
+              />
+              <ul className="list-unstyled">
+                {React.Children.toArray(children).filter(
+                  (child) =>
+                    !value || child.props.children.toLowerCase().startsWith(value),
+                )}
+              </ul>
+            </div>
+          );
+        },
+      );
+
     return (
         <div class="container">
             <div class="row align-items-start">
                 <div class="col">
                     <h1>Your Picks</h1>
-                    <a target="_blank" href="https://www.pdga.com/tour/event/77762" rel="noreferrer">Music City Open Link</a>
-                    <h3>Tournament Select:</h3>
+                    <a target="_blank" href="https://www.pdga.com/tour/event/77765" rel="noreferrer">Portland Open Link</a>
+                    <h3>Tournament Select:</h3>                    
                     <DropdownButton id="dropdown-basic-button" title="Tournament">
                         {
                             tourneys.map(tourney => {
@@ -138,19 +183,25 @@ function DGPicks(props) {
                         <div>
                             <h3>{"Your Selections for " + currTourney + ":"}</h3> 
                             {
-                                keys.map(index => 
-                                    <div key={index}>
-                                        <DropdownButton variant="outline-dark" id="dropdown-basic-button" title={currPicks[index] ? currPicks[index] : "Pick #" + index}>
-                                            {
-                                                allPlayers.map(player => {
-                                                    return <Dropdown.Item key={player} onClick={e=>handleNewPick(player, index)}>{player}</Dropdown.Item>
-                                                })
-                                            }
-                                        </DropdownButton>
+                                keys.map(index => {
+                                    return <div key={index}>
+                                        <Dropdown>
+                                            <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
+                                            {currPicks[index] ? currPicks[index] : "Pick #" + index}
+                                            </Dropdown.Toggle>
+
+                                            <Dropdown.Menu as={CustomMenu}>
+                                                {
+                                                    allPlayers.map(player => {
+                                                        return <Dropdown.Item key={player} onClick={e=>handleNewPick(player, index)}>{player}</Dropdown.Item>
+                                                    })
+                                                }
+                                            </Dropdown.Menu>
+                                        </Dropdown>
                                         <p>Current Selection: {savedPicks[index-1]}</p>
                                         <br/>
                                     </div>
-                                )
+                                })
                             }
                             <Button variant="success" disabled={tourneyStartDate[currTourney] < Date.now()} onClick={e => handleSubmit(currTourney, currPicks)}>Submit Picks</Button>
                         </div>
@@ -160,8 +211,23 @@ function DGPicks(props) {
                 <div class="col">
                     <h2>Previous Selections</h2>
                     {
-                        pickedPlayers.map(player => {
-                            return <p key={player}>{player} has been selected {prevSelections[player]} times</p>
+                        pickedPlayers.sort((a, b) => {
+                            const valueA = prevSelections[a];
+                            const valueB = prevSelections[b];
+                            if (valueA > valueB) {
+                                return -1; // a comes before b
+                            } else if (valueA < valueB) {
+                                return 1; // a comes after b
+                            } else {
+                                return 0; // a and b are equal
+                            }
+                        }).map(player => {
+                            if (prevSelections[player] === 3) {
+                                return <p key={player} style={{"color": "red"}}><strong>{player} has been selected {prevSelections[player]} times</strong></p>
+                            }
+                            else {
+                                return <p key={player}>{player} has been selected {prevSelections[player]} times</p>
+                            }
                         })
                     }
                 </div>
